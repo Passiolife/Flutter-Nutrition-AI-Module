8e9ecb5de7d74eda1b1aefe2aeb4e6f4586af7ab
Sahil Hamirani <sahil.hamirani@mindinventory.com>
2024-06-06 15:52:53 +0530

    - docs: Updated documentation. - chore: Updated dependencies.

--- lib/src/common/models/user_profile/user_profile_model.dart
+++ lib/src/common/models/user_profile/user_profile_model.dart
@@ -24,64 +24,123 @@ enum MeasurementSystem {
   imperial,
 }
 
+/// Represents units for weight measurements.
 enum WeightUnits {
+  /// Pounds (lbs).
   lbs,
+
+  /// Kilograms (kg).
   kg,
 }
 
+/// Represents units for water volume measurements.
 enum WaterUnits {
+  /// Ounces (oz).
   oz,
+
+  /// Milliliters (ml).
   ml,
 }
 
+/// Represents units for height measurements.
 enum HeightUnit {
+  /// Meters (m).
   meter,
+
+  /// Centimeters (cm).
   centimeter,
+
+  /// Feet (ft).
   feet,
+
+  /// Inches (in).
   inches,
 }
 
+/// Represents the gender selection options.
 enum GenderSelection {
+  /// Male gender.
   male,
+
+  /// Female gender.
   female,
 }
 
 // Define an enum to represent different conversion factors
 enum Conversion {
+  /// Conversion factor from centimeters to meters.
   centiMeterToMeter(100),
+
+  /// Conversion factor from meters to inches.
   meterToInch(39.3701),
+
+  /// Conversion factor from feet to inches.
   feetToInch(12),
+
+  /// Conversion factor from kilograms to pounds.
   kgToLbs(2.2046),
+
+  /// Conversion factor from milliliters to ounces.
   mlToOz(0.0338);
 
+  /// The value of the conversion factor.
   final double value;
 
   // Constructor to initialize the value of each conversion factor
   const Conversion(this.value);
 }
 
+/// Represents different levels of activity.
 enum ActivityLevel {
+  /// Not active.
   notActive,
+
+  /// Lightly active.
   lightlyActive,
+
+  /// Moderately active.
   moderatelyActive,
+
+  /// Very active.
   active;
 }
 
+/// Represents different options for calorie deficit or surplus.
 enum CalorieDeficit {
+  /// Lose 0.5 lbs per week.
   lose0_5(0.5),
+
+  /// Lose 1.0 lbs per week.
   lose1_0(1.0),
+
+  /// Lose 1.5 lbs per week.
   lose1_5(1.5),
+
+  /// Lose 2.0 lbs per week.
   lose2_0(2.0),
+
+  /// Gain 0.5 lbs per week.
   gain0_5(0.5),
+
+  /// Gain 1.0 lbs per week.
   gain1_0(1.0),
+
+  /// Gain 1.5 lbs per week.
   gain1_5(1.5),
+
+  /// Gain 2.0 lbs per week.
   gain2_0(2.0),
+
+  /// Maintain current weight (no deficit or surplus).
   maintainWeight(0);
 
+  /// The value of the calorie deficit or surplus.
   final double value;
 
+  /// Constructor to initialize the value of each calorie deficit or surplus.
   const CalorieDeficit(this.value);
 
+  /// Get the value of the calorie deficit or surplus based on the provided measurement system.
   double getValue(MeasurementSystem unit) {
     return (unit == MeasurementSystem.imperial)
         ? value
@@ -89,6 +148,7 @@ enum CalorieDeficit {
   }
 }
 
+/// A class representing a user's profile model.
 @JsonSerializable(explicitToJson: true)
 class UserProfileModel {
   UserProfileModel({
@@ -112,13 +172,17 @@ class UserProfileModel {
         _targetWater = targetWater,
         _calorieDeficit = calorieDeficit;
 
+  /// Unique identifier for the user profile.
   String? id;
+
+  /// Name of the user.
   String? name;
 
   // Age
   @JsonKey(includeFromJson: true, includeToJson: true)
   int? _age;
 
+  /// Set the age of the user.
   void setAge(int? age) {
     if (_age == age) return;
     _age = age;
@@ -128,12 +192,14 @@ class UserProfileModel {
     }
   }
 
+  /// Get the age of the user.
   int? getAge() => _age;
 
   // Weight
   @JsonKey(includeFromJson: true, includeToJson: true)
   double? _weight;
 
+  /// Set the weight of the user.
   void setWeight(double? weight) {
     if (_weight == weight) return;
     _weight = switch (weightUnit) {
@@ -146,6 +212,7 @@ class UserProfileModel {
     }
   }
 
+  /// Get the weight of the user.
   double? getWeight() {
     if (_weight != null) {
       return switch (weightUnit) {
@@ -156,19 +223,26 @@ class UserProfileModel {
     return null;
   }
 
+  /// Stores the target weight
   @JsonKey(includeFromJson: true, includeToJson: true)
   double _targetWeight;
 
+  /// Sets the target weight based on the provided weight and the current measurement system.
   void setTargetWeight(double weight) {
     _targetWeight = switch (weightUnit) {
+      // If the measurement system is imperial, convert weight to kilograms.
       MeasurementSystem.imperial => weight / Conversion.kgToLbs.value,
+      // If the measurement system is metric, keep weight as it is.
       _ => weight,
     };
   }
 
+  /// Gets the target weight based on the current measurement system.
   double? getTargetWeight() {
     return switch (weightUnit) {
+      // If the measurement system is imperial, convert weight back to pounds.
       MeasurementSystem.imperial => _targetWeight * Conversion.kgToLbs.value,
+      // If the measurement system is metric, return the weight as it is.
       _ => _targetWeight,
     };
   }
@@ -177,40 +251,53 @@ class UserProfileModel {
   MeasurementSystem heightUnit;
   MeasurementSystem weightUnit;
 
+  /// Stores the target water volume.
   @JsonKey(includeFromJson: true, includeToJson: true)
   double _targetWater;
 
+  /// Sets the target water volume based on the provided volume and the current measurement system.
   void setTargetWater(double water) {
     _targetWater = switch (weightUnit) {
+      // If the measurement system is imperial, convert water volume to ounces.
       MeasurementSystem.imperial => water / Conversion.mlToOz.value,
+      // If the measurement system is metric, keep water volume as it is.
       _ => water,
     };
   }
 
+  /// Gets the target water volume based on the current measurement system.
   double? getTargetWater() {
     return switch (weightUnit) {
+      // If the measurement system is imperial, convert water volume back to milliliters and format the result.
       MeasurementSystem.imperial =>
         (_targetWater * Conversion.mlToOz.value).parseFormatted(),
+      // If the measurement system is metric, return the water volume as it is and format the result.
       _ => _targetWater.parseFormatted(),
     };
   }
 
-  // ActivityLevel
+  // Stores the user's activity level.
   @JsonKey(includeFromJson: true, includeToJson: true)
   ActivityLevel? _activityLevel;
 
+  /// Sets the user's activity level and updates the recommended calorie target if needed.
   void setActivityLevel(ActivityLevel? activityLevel) {
+    // If the new activity level is the same as the current one, do nothing.
     if (_activityLevel == activityLevel) return;
+    // Set the new activity level.
     _activityLevel = activityLevel;
+    // Recalculate the recommended calorie target.
     int calories = _calculateRecommendedCalorie();
     if (calories > 0) {
+      // Update the calorie target if it's positive.
       caloriesTarget = calories;
     }
   }
 
+  /// Gets the user's activity level.
   ActivityLevel? getActivityLevel() => _activityLevel;
 
-  // Height
+  /// Stores the user's height.
   @JsonKey(includeFromJson: true, includeToJson: true)
   double? _height;
 
@@ -229,9 +316,11 @@ class UserProfileModel {
     }
   }
 
+  /// Gets the user's height.
   double? getHeight() => _height;
 
-  // Method to get the height in the specified measurement system
+  /// Gets the height in the specified measurement system.
+  /// Returns a tuple containing the unit and subunit (e.g., unit = meters, subunit = centimeters for metric).
   ({int unit, int subunit}) get heightInMeasurementSystem {
     switch (heightUnit) {
       case MeasurementSystem.metric:
@@ -256,6 +345,7 @@ class UserProfileModel {
     }
   }
 
+  /// Gets the description of the user's height based on the measurement system.
   String get heightDescription {
     ({int unit, int subunit}) height = heightInMeasurementSystem;
     switch (heightUnit) {
@@ -266,102 +356,163 @@ class UserProfileModel {
     }
   }
 
+  /// Stores the user's selected calorie deficit.
   @JsonKey(includeFromJson: true, includeToJson: true)
   CalorieDeficit _calorieDeficit;
 
+  /// Sets the user's selected calorie deficit and updates the recommended calorie target if needed.
+  void setCalorieDeficit(CalorieDeficit calorieDeficit) {
+    // If the new calorie deficit is the same as the current one, do nothing.
+    if (_calorieDeficit == calorieDeficit) return;
+    // Set the new calorie deficit.
+    _calorieDeficit = calorieDeficit;
+    // Recalculate the recommended calorie target.
+    int calories = _calculateRecommendedCalorie();
+    if (calories > 0) {
+      // Update the calorie target if it's positive.
+      caloriesTarget = calories;
+    }
+  }
+
+  /// Gets the user's selected calorie deficit.
+  CalorieDeficit getCalorieDeficit() => _calorieDeficit;
+
+  /// Stores the user's meal plan.
   @JsonKey(includeFromJson: true, includeToJson: true)
   PassioMealPlan? _mealPlan;
 
+  /// Sets the user's meal plan and updates the nutrient percentages accordingly.
   void setPassioMealPlan(PassioMealPlan mealPlan) {
+    // Set the new meal plan.
     _mealPlan = mealPlan;
+    // Update the nutrient percentages based on the meal plan targets.
     setCarbsPercentage(mealPlan.carbTarget);
     setProteinPercentage(mealPlan.proteinTarget);
     setFatPercentage(mealPlan.fatTarget);
   }
 
+  /// Gets the user's meal plan.
   PassioMealPlan? getPassioMealPlan() => _mealPlan;
 
-  void setCalorieDeficit(CalorieDeficit calorieDeficit) {
-    if (_calorieDeficit == calorieDeficit) return;
-    _calorieDeficit = calorieDeficit;
-    int calories = _calculateRecommendedCalorie();
-    if (calories > 0) {
-      caloriesTarget = calories;
-    }
-  }
-
-  CalorieDeficit getCalorieDeficit() => _calorieDeficit;
-
+  /// Stores the target daily calorie intake.
   int caloriesTarget = 2100;
 
+  /// Gets the recommended daily calorie intake based on the user's profile.
   int get recommendedCalories => _calculateRecommendedCalorie();
 
-  // Carbs
+  /// Stores the percentage of carbohydrates in the diet.
   int carbsPercentage = 50;
 
+  /// Calculates the total grams of carbohydrates based on the percentage and total calorie target.
   int get carbsGram => caloriesTarget * carbsPercentage / 100 ~/ 4;
 
+  /// Sets the percentage of carbohydrates in the diet and adjusts other nutrient percentages to maintain balance.
   void setCarbsPercentage(int carbs) {
     final values = (balance3Values(
         first: carbs, second: proteinPercentage, third: fatPercentage));
+    // Set the new percentage of carbohydrates.
     carbsPercentage = values.$1;
+    // Set the adjusted percentage of protein.
     proteinPercentage = values.$2;
+    // Set the adjusted percentage of fat.
     fatPercentage = values.$3;
   }
 
+  /// Stores the percentage of protein in the diet.
   int proteinPercentage = 25;
 
+  /// Calculates the total grams of protein based on the percentage and total calorie target.
   int get proteinGram => caloriesTarget * proteinPercentage / 100 ~/ 4;
 
+  /// Sets the percentage of protein in the diet and adjusts other nutrient percentages to maintain balance.
   void setProteinPercentage(int protein) {
     final values = (balance3Values(
         first: protein, second: fatPercentage, third: carbsPercentage));
+    // Set the new percentage of protein.
     proteinPercentage = values.$1;
+    // Set the adjusted percentage of fat.
     fatPercentage = values.$2;
+    // Set the adjusted percentage of carbohydrates.
     carbsPercentage = values.$3;
   }
 
+  /// Stores the percentage of fat in the diet.
   int fatPercentage = 25;
 
+  /// Calculates the total grams of fat based on the percentage and total calorie target.
   int get fatGram => caloriesTarget * fatPercentage / 100 ~/ 9;
 
+  /// Sets the percentage of fat in the diet and adjusts other nutrient percentages to maintain balance.
   void setFatPercentage(int fat) {
     final values = (balance3Values(
         first: fat, second: proteinPercentage, third: carbsPercentage));
+    // Set the new percentage of fat.
     fatPercentage = values.$1;
+    // Set the adjusted percentage of protein.
     proteinPercentage = values.$2;
+    // Set the adjusted percentage of carbohydrates.
     carbsPercentage = values.$3;
   }
 
+  /// Balances three values ensuring they sum up to 100.
+  ///
+  /// This function adjusts the first value to ensure that the sum of all three values is 100.
+  /// If the sum of the first and third values exceeds 100, the third value is set to 0 and the remaining percentage is distributed between the first and second values.
+  /// If the sum is less than 100, the remaining percentage is assigned to the second value.
+  ///
+  /// Returns a tuple containing the balanced values.
   (int, int, int) balance3Values(
       {required int first, required int second, required int third}) {
+    // Validate the first value.
     final validateFirst = _validatePercent(value: first);
     if ((validateFirst + third) > 100) {
+      // If the sum of the first and third values exceeds 100, adjust the values.
       return (validateFirst, 0, 100 - validateFirst);
     } else {
+      // If the sum is less than 100, adjust the values accordingly.
       return (validateFirst, 100 - validateFirst - third, third);
     }
   }
 
+  /// Validates a percentage value to ensure it falls within the range of 0 to 100.
+  ///
+  /// If the value is greater than 100, it's capped at 100.
+  /// If the value is less than 0, it's set to 0.
+  ///
+  /// Returns the validated percentage value.
   int _validatePercent({required int value}) {
     return switch (value) {
+      // If the value is greater than 100, return 100.
       > 100 => 100,
+      // If the value is less than 0, return 0.
       < 0 => 0,
+      // Otherwise, return the value as is.
       _ => value,
     };
   }
 
-  /// Function to calculate Basal Metabolic Rate (BMR)
+  /// Calculates Basal Metabolic Rate (BMR) and activity-adjusted calorie intake based on user profile.
+  ///
+  /// This function calculates the BMR using the Harris-Benedict equation and adjusts it based on the user's activity level.
+  /// It then further adjusts the calorie intake based on the desired calorie deficit or surplus.
+  ///
+  /// Returns a tuple containing BMR and activity level.
   ({double? bmr, ActivityLevel? activityLevel}) _calculateBMR() {
     if (_activityLevel == null ||
         _age == null ||
         _height == null ||
-        _weight == null) return (bmr: null, activityLevel: null);
+        _weight == null) {
+      // Return null if essential profile data is missing.
+      return (bmr: null, activityLevel: null);
+    }
+
+    // Initialize BMR variable.
     double bmr = 0;
+    // Convert height to meters.
     double heightInMeter = _height! * Conversion.centiMeterToMeter.value;
     // Calculate weight in kilograms
     double weightInKg = 10 * _weight!;
-    // Calculate BMR based on gender
+    // Calculate BMR based on gender using the Harris-Benedict equation.
     bmr = switch (gender) {
       GenderSelection.male => weightInKg +
           (6.25 * heightInMeter) -
@@ -372,55 +523,79 @@ class UserProfileModel {
           (5 * _age!) -
           161, // Formula for BMR calculation for females
     };
+    // Return calculated BMR and activity level.
     return (bmr: bmr, activityLevel: _activityLevel);
   }
 
+  /// Calculates the total calorie intake based on BMR and activity level.
   int _calculateCaloriesBasedOnActivityLevel() {
+    // Calculate BMR.
     final bmr = _calculateBMR();
-    if (bmr.bmr == null) return 0;
+
+    if (bmr.bmr == null) return 0; // Return 0 if BMR is null.
+
+    // Adjust BMR based on activity level.
     return switch (bmr.activityLevel) {
       ActivityLevel.notActive => ((bmr.bmr!) * 1.2).toInt(),
       ActivityLevel.lightlyActive => ((bmr.bmr!) * 1.375).toInt(),
       ActivityLevel.moderatelyActive => ((bmr.bmr!) * 1.55).toInt(),
       ActivityLevel.active => ((bmr.bmr!) * 1.725).toInt(),
-      _ => 2100,
+      _ => 2100, // Default calorie intake.
     };
   }
 
+  /// Calculates the recommended daily calorie intake based on user profile and desired calorie deficit or surplus.
   int _calculateRecommendedCalorie() {
+    // Get calorie intake based on activity level.
     int calories = _calculateCaloriesBasedOnActivityLevel();
     return switch (_calorieDeficit) {
-      CalorieDeficit.lose0_5 => calories -= 250,
-      CalorieDeficit.lose1_0 => calories -= 500,
-      CalorieDeficit.lose1_5 => calories -= 750,
-      CalorieDeficit.lose2_0 => calories -= 1000,
-      CalorieDeficit.gain0_5 => calories += 250,
-      CalorieDeficit.gain1_0 => calories += 500,
-      CalorieDeficit.gain1_5 => calories += 750,
-      CalorieDeficit.gain2_0 => calories += 1000,
-      CalorieDeficit.maintainWeight => calories += 0,
+      CalorieDeficit.lose0_5 => calories -=
+          250, // Reduce calories by 250 for a deficit of 0.5 kg per week.
+      CalorieDeficit.lose1_0 => calories -=
+          500, // Reduce calories by 500 for a deficit of 1 kg per week.
+      CalorieDeficit.lose1_5 => calories -=
+          750, // Reduce calories by 750 for a deficit of 1.5 kg per week.
+      CalorieDeficit.lose2_0 => calories -=
+          1000, // Reduce calories by 1000 for a deficit of 2 kg per week.
+      CalorieDeficit.gain0_5 => calories +=
+          250, // Increase calories by 250 for a surplus of 0.5 kg per week.
+      CalorieDeficit.gain1_0 => calories +=
+          500, // Increase calories by 500 for a surplus of 1 kg per week.
+      CalorieDeficit.gain1_5 => calories +=
+          750, // Increase calories by 750 for a surplus of 1.5 kg per week.
+      CalorieDeficit.gain2_0 => calories +=
+          1000, // Increase calories by 1000 for a surplus of 2 kg per week.
+      CalorieDeficit.maintainWeight => calories +=
+          0, // Maintain current weight.
     };
   }
 
+  /// Calculates Body Mass Index (BMI) based on weight and height.
   double? get bmi {
     if (_weight != null &&
         (_weight ?? 0) > 0 &&
         _height != null &&
         (_height ?? 0) > 0) {
+      // Calculate BMI using weight (kg) divided by height squared (m^2).
       return ((_weight ?? 0) / pow((_height ?? 0), 2)).parseFormatted();
     }
+    // Return null if weight or height is missing or invalid.
     return null;
   }
 
+  /// Returns a description of BMI category based on calculated BMI value.
   String get bmiDescription {
     return switch (bmi ?? 0) {
-      < 18.5 => 'Underweight',
-      >= 18.5 && < 25 => 'Normal',
-      >= 25 && < 30 => 'Overweight',
-      _ => 'Obese',
+      < 18.5 => 'Underweight', // BMI less than 18.5 indicates underweight.
+      >= 18.5 && < 25 =>
+        'Normal', // BMI between 18.5 and 24.9 indicates normal weight.
+      >= 25 && < 30 =>
+        'Overweight', // BMI between 25 and 29.9 indicates overweight.
+      _ => 'Obese', // BMI of 30 or greater indicates obesity.
     };
   }
 
+  /// Overrides the equality operator.
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
@@ -440,6 +615,7 @@ class UserProfileModel {
         other._calorieDeficit == _calorieDeficit;
   }
 
+  /// Overrides the hashCode method.
   @override
   int get hashCode => Object.hash(
         id,
@@ -456,8 +632,10 @@ class UserProfileModel {
         _calorieDeficit,
       );
 
+  /// Constructs a [UserProfileModel] instance from a JSON map.
   factory UserProfileModel.fromJson(Map<String, dynamic> json) =>
       _$UserProfileModelFromJson(json);
 
+  /// Converts the [UserProfileModel] instance to a JSON map.
   Map<String, dynamic> toJson() => _$UserProfileModelToJson(this);
 }
